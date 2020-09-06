87262d002cd4589ae89dabff54e5b6710e20d1b5
Yujeonja <hardboiled65@gmail.com>
2020-09-06 15:29:32 +0900

    Now dock window is not showing.

--- Dock.cpp
+++ Dock.cpp
@@ -428,6 +428,7 @@ bool Dock::is_normal_window(unsigned long w_id) const
     unsigned char *ret;
     int result;
     Atom type_normal;
+    Atom type_dock;
 
     // Get property.
     prop = XInternAtom(this->_dpy, "_NET_WM_WINDOW_TYPE", False);
@@ -452,9 +453,14 @@ bool Dock::is_normal_window(unsigned long w_id) const
         return true;
     }
 
+    type_dock = XInternAtom(this->_dpy, "_NET_WM_WINDOW_TYPE_DOCK", False);
     type_normal = XInternAtom(this->_dpy, "_NET_WM_WINDOW_TYPE_NORMAL", False);
 
     for (unsigned long i = 0; i < n_items; ++i) {
+        if (((Atom*)ret)[i] == type_dock) {
+            XFree(ret);
+            return false;
+        }
         if (((Atom*)ret)[i] == type_normal) {
             XFree(ret);
             return true;
