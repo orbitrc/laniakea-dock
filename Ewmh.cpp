84767ee02d48a7bc93c7cb0b481ec598a8ca00c8
Yujeonja <hardboiled65@gmail.com>
2020-09-08 13:37:52 +0900

    Ewmh send event added.

--- Ewmh.cpp
+++ Ewmh.cpp
@@ -10,6 +10,47 @@ Ewmh::Ewmh()
 // Public functions
 //=======================
 
+bool Ewmh::send_net_close_window(uint32_t w)
+{
+    xcb_connection_t *conn = xcb_connect(NULL, NULL);
+
+    const xcb_setup_t *setup = xcb_get_setup(conn);
+    xcb_screen_iterator_t iter = xcb_setup_roots_iterator(setup);
+    xcb_screen_t *screen = iter.data;
+
+    xcb_intern_atom_cookie_t intern_cookie = xcb_intern_atom(conn, 1, strlen("_NET_CLOSE_WINDOW"), "_NET_CLOSE_WINDOW");
+    xcb_intern_atom_reply_t *intern_reply = xcb_intern_atom_reply(conn, intern_cookie, NULL);
+    xcb_atom_t net_close_window = intern_reply->atom;
+    free(intern_reply);
+
+    xcb_client_message_event_t evt;
+    evt.response_type = XCB_CLIENT_MESSAGE;
+    evt.format = 32;
+    evt.window = w;
+    evt.type = net_close_window;
+    evt.data.data32[0] = 0;
+    evt.data.data32[1] = 2;
+    evt.data.data32[2] = 0;
+    evt.data.data32[3] = 0;
+    evt.data.data32[4] = 0;
+
+    auto cookie = Ewmh::send_event(conn,
+        screen->root,
+        XCB_EVENT_MASK_SUBSTRUCTURE_REDIRECT,
+        (const char*)&evt
+    );
+
+    xcb_generic_error_t *err = xcb_request_check(conn, cookie);
+    if (err) {
+        xcb_disconnect(conn);
+        return false;
+    }
+
+    xcb_disconnect(conn);
+
+    return true;
+}
+
 QString Ewmh::get_net_wm_name(uint32_t w)
 {
     xcb_connection_t *conn = xcb_connect(NULL, NULL);
@@ -71,6 +112,15 @@ xcb_get_property_cookie_t Ewmh::get_property(xcb_connection_t *conn,
         0,
         1024
     );
+
+    return cookie;
+}
+
+xcb_void_cookie_t Ewmh::send_event(xcb_connection_t *conn,
+        xcb_window_t w, uint32_t event_mask, const char *event)
+{
+    xcb_void_cookie_t cookie = xcb_send_event_checked(conn, 0, w, event_mask, event);
+    xcb_flush(conn);
 
     return cookie;
 }
--- Ewmh.h
+++ Ewmh.h
@@ -3,6 +3,8 @@
 
 #include <QString>
 
+#include <stdint.h>
+
 #include <xcb/xcb.h>
 
 class Ewmh
@@ -10,12 +12,23 @@ class Ewmh
 public:
     Ewmh();
 
+    //=================================
+    // Other root window messages
+    //=================================
+    static bool send_net_close_window(uint32_t w);
+
+    //=================================
+    // Application window properties
+    //=================================
     static QString get_net_wm_name(uint32_t w);
     static uint32_t get_net_wm_desktop(uint32_t w);
 
 private:
     static xcb_get_property_cookie_t get_property(xcb_connection_t *conn,
             xcb_window_t w, const QString& prop, xcb_atom_t type);
+
+    static xcb_void_cookie_t send_event(xcb_connection_t *conn,
+            xcb_window_t w, uint32_t event_mask, const char *event);
 };
 
 #endif // _LA_DOCK_EWMH_H
